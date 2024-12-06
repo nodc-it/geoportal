abbf069dbf59d9f2029fdc95047711ae269fc425
pscarascia <156223857+pscarascia@users.noreply.github.com>
2024-12-06 15:12:27 +0100

    centratura mappa da pagina in tempo reale + ottimizzazione codice
    Modifiche per:
    - acquisizione parametro URL per centratura mappa su zona desiderata e relativi layers
    - ottimizzazione codice data-table component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OlMapComponent } from './ol-map/ol-map.component';
+
+
+const routes: Routes = 
+[
+	// path for msv URL parameter
+	// to differentiate map view
+	// from real-time data page in OGS website
+	{ path: 'OlMapComponent/:msv', component: OlMapComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
